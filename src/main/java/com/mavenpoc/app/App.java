9361a2d9ca39437fa81ff22b59438bb34d098909
Isuruwan Herath <isuruwan.herath@infor.com>
2017-09-29 18:24:46 +0530

    starting again

---
+++ src/main/java/com/mavenpoc/app/App.java
@@ -0,0 +1,13 @@
+package com.mavenpoc.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
